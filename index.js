b9b83073248682b62900a10530d5dd73a63efb91
Eduardo Jirón <emjironal@gmail.com>
2020-07-29 13:59:21 -0600

    [new] Node.js project struture

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path');
+const csv = require('csvtojson')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views'); 
+app.use(express.static(path.join(__dirname, 'public')))
+
+function getMain(req, res)
+{
+	res.render("pages/index.ejs") 
+}
+
+app.get('/', getMain)
+app.get('/getHappiness', getHappiness)
+
+app.listen(PORT, ()=>
+{
+    console.log('Server started at: http://localhost:'+ PORT)
+}) //end listen
