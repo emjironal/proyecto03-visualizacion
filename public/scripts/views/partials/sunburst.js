647c9b3aacec92767801a4cbd3eab4a526412b85
Eduardo Jirón <emjironal@gmail.com>
2020-08-06 21:58:31 -0600

    [updated] Avance con el gráfico sunburst, falta agregar información al gráfico

---
+++ public/scripts/views/partials/sunburst.js
@@ -0,0 +1,55 @@
+HighCharts.chart("sunburst",
+    {
+        chart: {
+            height: "100%"
+        },
+        title: {
+            text: "Actividades lúdicas en CR",
+        },
+        subtitle: {
+            text: "Clasificaciones"
+        },
+        series: [{
+            type : "sunburst",
+            //data: data
+            allowDrillToNode: true,
+            cursor: "pointer",
+            datalabels: {
+                //format: "{"
+                filter: {
+                    property: "innerArcLenght",
+                    operator: ">",
+                    value: 16
+                },
+                rotationMode: circular
+                },
+                levels: [{
+                    level: 1,
+                    levelIsConstant: false,
+                    dataLabels: {
+                        filter: {
+                            property: 'outerArcLength',
+                            operator: '>',
+                            value: 64
+                        }
+                    }
+                }, {
+                    level: 2,
+                    colorByPoint: true
+                },
+                {
+                    level: 3,
+                    colorVariation: {
+                        key: 'brightness',
+                        to: -0.5
+                    }
+                }, {
+                    level: 4,
+                    colorVariation: {
+                        key: 'brightness',
+                        to: 0.5
+                    }
+                }]
+            }]
+    }
+)
