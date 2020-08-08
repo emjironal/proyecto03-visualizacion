1f70f23d08f5b97d306fa57d376a0a42108c5cc1
Eduardo Jirón <emjironal@gmail.com>
2020-08-08 16:46:35 -0600

    [updated] Cambio en la interfaz gráfica

--- public/scripts/views/partials/sunburst.js
+++ public/scripts/views/partials/sunburst.js
@@ -1,55 +1,58 @@
-HighCharts.chart("sunburst",
-    {
-        chart: {
-            height: "100%"
-        },
-        title: {
-            text: "Actividades lúdicas en CR",
-        },
-        subtitle: {
-            text: "Clasificaciones"
-        },
-        series: [{
-            type : "sunburst",
-            //data: data
-            allowDrillToNode: true,
-            cursor: "pointer",
-            datalabels: {
-                //format: "{"
-                filter: {
-                    property: "innerArcLenght",
-                    operator: ">",
-                    value: 16
-                },
-                rotationMode: circular
-                },
-                levels: [{
-                    level: 1,
-                    levelIsConstant: false,
-                    dataLabels: {
-                        filter: {
-                            property: 'outerArcLength',
-                            operator: '>',
-                            value: 64
-                        }
+Highcharts.chart("sunburst",
+{
+    chart: {
+        height: "100%"
+    },
+    title: {
+        text: "Actividades lúdicas en CR",
+    },
+    subtitle: {
+        text: "Clasificaciones"
+    },
+    series: [{
+        type : "sunburst",
+        //data: data
+        allowDrillToNode: true,
+        cursor: "pointer",
+        datalabels: {
+            //format: "{"
+            filter: {
+                property: "innerArcLenght",
+                operator: ">",
+                value: 16
+            },
+            rotationMode: "circular"
+            },
+            levels: [{
+                level: 1,
+                levelIsConstant: false,
+                dataLabels: {
+                    filter: {
+                        property: 'outerArcLength',
+                        operator: '>',
+                        value: 64
                     }
-                }, {
-                    level: 2,
-                    colorByPoint: true
-                },
-                {
-                    level: 3,
-                    colorVariation: {
-                        key: 'brightness',
-                        to: -0.5
-                    }
-                }, {
-                    level: 4,
-                    colorVariation: {
-                        key: 'brightness',
-                        to: 0.5
-                    }
-                }]
+                }
+            }, {
+                level: 2,
+                colorByPoint: true
+            },
+            {
+                level: 3,
+                colorVariation: {
+                    key: 'brightness',
+                    to: -0.5
+                }
+            }, {
+                level: 4,
+                colorVariation: {
+                    key: 'brightness',
+                    to: 0.5
+                }
             }]
+        }],
+    tooltip: {
+        headerFormat: "",
+        pointFormat: "Personas que realizan la actividad lúdica"
     }
-)
+})
