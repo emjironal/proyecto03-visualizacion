d6a6eb456077236eac6b473ee49a689050c2f589
Eduardo Jirón <emjironal@gmail.com>
2020-08-09 09:31:07 -0600

    [updated] Cambio en manera de crear gráficos y select gráfico

---
+++ public/scripts/controller.js
@@ -0,0 +1,12 @@
+function selectChart(chart)
+{
+    if(chart == "sunburst")
+    {
+        sunburstChart([]);
+    }
+    else
+    {
+        
+    }
+}
+selectChart("sunburst")
--- public/scripts/views/partials/sunburst.js
+++ public/scripts/views/partials/sunburst.js
@@ -1,58 +1,61 @@
-Highcharts.chart("sunburst",
+function sunburstChart(data)
 {
-    chart: {
-        height: "100%"
-    },
-    title: {
-        text: "Actividades lúdicas en CR",
-    },
-    subtitle: {
-        text: "Clasificaciones"
-    },
-    series: [{
-        type : "sunburst",
-        //data: data
-        allowDrillToNode: true,
-        cursor: "pointer",
-        datalabels: {
-            //format: "{"
-            filter: {
-                property: "innerArcLenght",
-                operator: ">",
-                value: 16
-            },
-            rotationMode: "circular"
-            },
-            levels: [{
-                level: 1,
-                levelIsConstant: false,
-                dataLabels: {
-                    filter: {
-                        property: 'outerArcLength',
-                        operator: '>',
-                        value: 64
+    Highcharts.chart("chart",
+    {
+        chart: {
+            height: "100%"
+        },
+        title: {
+            text: "Actividades lúdicas en CR",
+        },
+        subtitle: {
+            text: "Clasificaciones"
+        },
+        series: [{
+            type : "sunburst",
+            data: data,
+            allowDrillToNode: true,
+            cursor: "pointer",
+            datalabels: {
+                //format: "{"
+                filter: {
+                    property: "innerArcLenght",
+                    operator: ">",
+                    value: 16
+                },
+                rotationMode: "circular"
+                },
+                levels: [{
+                    level: 1,
+                    levelIsConstant: false,
+                    dataLabels: {
+                        filter: {
+                            property: 'outerArcLength',
+                            operator: '>',
+                            value: 64
+                        }
                     }
-                }
-            }, {
-                level: 2,
-                colorByPoint: true
-            },
-            {
-                level: 3,
-                colorVariation: {
-                    key: 'brightness',
-                    to: -0.5
-                }
-            }, {
-                level: 4,
-                colorVariation: {
-                    key: 'brightness',
-                    to: 0.5
-                }
-            }]
-        }],
-    tooltip: {
-        headerFormat: "",
-        pointFormat: "Personas que realizan la actividad lúdica"
-    }
-})
+                }, {
+                    level: 2,
+                    colorByPoint: true
+                },
+                {
+                    level: 3,
+                    colorVariation: {
+                        key: 'brightness',
+                        to: -0.5
+                    }
+                }, {
+                    level: 4,
+                    colorVariation: {
+                        key: 'brightness',
+                        to: 0.5
+                    }
+                }]
+            }],
+        tooltip: {
+            headerFormat: "",
+            pointFormat: "Personas que realizan la actividad lúdica"
+        }
+    })
+}
